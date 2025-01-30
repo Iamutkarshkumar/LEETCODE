7f2642d99c6177fb43bb4e3c2c50539cfa9d07af
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-01-30 19:03:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (16.45%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int i='A'-'0';
+        int j='Z'-'0';
+
+        int small=0;
+        int cap=0;
+        for(auto ele: word) {
+            if(ele-'0'>=i and ele-'0'<=j) cap++;
+            else small++;
+        }
+        int n=word.length();
+        if(cap==n or small==n) return true;
+        if(cap==1 and small==n-1 and (word[0]-'0'>=i and word[0]-'0'<=j)) return true;
+        return false;
+    }
+};
