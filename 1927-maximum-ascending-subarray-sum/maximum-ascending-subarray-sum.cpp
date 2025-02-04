3954229bf6cd7c00ac349f7c70a494e2ec0dd579
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-02-04 08:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (47.21%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int maxSum=INT_MIN;
+        int maxsum=0;
+        int sum=nums[0];
         int n=nums.size();
-        vector<int> help;
-        if(n==1) return nums[0];
-        int i=0;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int sum=nums[i];
-            int j=i+1;
-            while(j<n and nums[j]>nums[j-1]){
-                sum+=nums[j];
-                j++;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]) sum+=nums[i];
+            else{
+                maxsum=max(maxsum,sum);
+                sum=nums[i];
             }
-            maxSum=max(sum,maxSum);
         }
-        return maxSum;
-
+        return max(maxsum,sum);
     }
 };
+// class Solution {
+// public:
+//     int maxAscendingSum(vector<int>& nums) {
+//         int maxSum=INT_MIN;
+//         int n=nums.size();
+//         vector<int> help;
+//         if(n==1) return nums[0];
+//         int i=0;
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             int sum=nums[i];
+//             int j=i+1;
+//             while(j<n and nums[j]>nums[j-1]){
+//                 sum+=nums[j];
+//                 j++;
+//             }
+//             maxSum=max(sum,maxSum);
+//         }
+//         return maxSum;
+
+//     }
+// };
