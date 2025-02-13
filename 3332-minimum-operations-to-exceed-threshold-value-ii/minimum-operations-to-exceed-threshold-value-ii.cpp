2801b5a7240e4a3ceb1a533aedf38562dd643c71
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-02-13 09:01:21 +0530

    Time: 85 ms (92.04%) | Memory: 91.5 MB (82.49%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,7 +1,6 @@
 #define ll long long int
 class Solution {
 public:
-
     int minOperations(vector<int>& nums, int k) {
         ll n=nums.size();
         if(n==1) return 0;
@@ -11,7 +10,7 @@ public:
         while(pq.size()>1 and pq.top()<k){
             ll x=pq.top();pq.pop();
             ll y=pq.top();pq.pop();
-            pq.push(min(x, y) * 2 + max(x, y));
+            pq.push(x * 2 + y);
             count++;
         }
         return count;
