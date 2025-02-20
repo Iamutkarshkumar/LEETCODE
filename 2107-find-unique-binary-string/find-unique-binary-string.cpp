0666edd29ab83d4d818800326dc475b9b2fb0227
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-02-20 09:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (69.88%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,8 +4,7 @@ public:
         string ans;
         int x=0;
         for(auto ele: nums){
-            if(ele[x]=='1') ans+="0";
-            else ans+="1";
+            ele[x]=='1' ? ans+="0" : ans+="1";
             x++;
         }
         return ans;
