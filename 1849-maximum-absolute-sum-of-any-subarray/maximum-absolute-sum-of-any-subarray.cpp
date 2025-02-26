58750f2a74bff1cc9cc43930eb95304794e33b13
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-02-26 08:25:30 +0530

    Time: 3 ms (24.38%) | Memory: 45.1 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,30 +1,14 @@
-// class Solution {
-// public:
-//     int maxAbsoluteSum(vector<int>& nums) {
-//         int n=nums.size();
-//         int preSum=0,minSum=INT_MAX,maxSum=INT_MIN;
-
-//         for(auto ele: nums){
-//             preSum+=ele;
-//             minSum=min(minSum,preSum);
-//             maxSum=max(maxSum,preSum);
-//         } 
-//         return maxSum-minSum;
-//     }
-// };
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int minPrefixSum = 0, maxPrefixSum = 0;
+        int n=nums.size();
+        int preSum=0,minSum=0,maxSum=0;
 
-        int prefixSum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            prefixSum += nums[i];
-
-            minPrefixSum = min(minPrefixSum, prefixSum);
-            maxPrefixSum = max(maxPrefixSum, prefixSum);
-        }
-
-        return maxPrefixSum - minPrefixSum;
+        for(auto ele: nums){
+            preSum+=ele;
+            minSum=min(minSum,preSum);
+            maxSum=max(maxSum,preSum);
+        } 
+        return maxSum-minSum;
     }
 };
