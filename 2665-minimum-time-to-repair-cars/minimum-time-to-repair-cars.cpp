7632f3a4b0d59c162be3d1b8db19c65f5c28d160
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-03-16 09:30:27 +0530

    Time: 51 ms (58.38%) | Memory: 59.2 MB (96.42%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -4,7 +4,7 @@ public:
         long long mx=*max_element(ranks.begin(),ranks.end());
         long long l = 0;
         long long h = mx*cars*cars;
-        long long ans=LLONG_MAX;
+        long long ans=h;
 
         while(l<=h){
             long long m = l+(h-l)/2;
@@ -12,15 +12,11 @@ public:
             for(auto ele: ranks){
                 count+=sqrt(m/ele);
             }
-            if(count>cars){
+            if(count>=cars){
                 h=m-1;
                 ans=min(ans,m);
             }
-            else if(count<cars) l=m+1;
-            else{
-                ans=min(m,ans);
-                h=m-1;
-            }
+            else l=m+1;
         }
         return ans;
     }
