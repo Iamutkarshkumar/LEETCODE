da2202c048e6ebb347b02754a41534c55caccf9e
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-03-16 19:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.16%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l=0,h=x;
+        while(l<=h){
+            long long m=l+(h-l)/2;
+            if(m*m==x) return m;
+            else if(m*m>x) h=m-1;
+            else l=m+1;
+        }
+        return h;
+    }
+};
