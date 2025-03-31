ba5691666c5eb427d865352e4fd064501b53aab9
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-03-31 09:29:36 +0530

    Time: 3 ms (32.22%) | Memory: 10.6 MB (12.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // Here are the ASCII values:
+        //     'A' is 65
+        //     'Z' is 90
+        //     'a' is 97
+        //     'z' is 122
+        //     Whitespace (space) is 32
+
+        string help="";
+        for(auto ele: s){
+            if(ele-'A'>=0 and ele-'A'<=25) help.push_back(ele+32);
+            else if(ele-'a'>=0 and ele-'a'<=25) help.push_back(ele);
+            else if(ele-'a'>=-49 and ele-'a'<=-40) help.push_back(ele);
+            else continue;
+        }
+        string x=help;
+        reverse(begin(x),end(x));
+        int n=x.length();
+        for(int i=0;i<n/2;i++){
+            if(help[i]!=x[i]) return false;
+        }
+        return true;
+    }
+};
