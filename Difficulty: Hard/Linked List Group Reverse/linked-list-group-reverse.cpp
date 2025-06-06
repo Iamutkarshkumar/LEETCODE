3b3a8385e2658493d0ff845634295e8c31e437ca
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-06-06 07:47:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
@@ -18,25 +18,23 @@ class Solution {
     Node *reverseKGroup(Node *head, int k) {
         if (!head || k <= 1) 
             return head;
-        
-        // 1) collect pointers to all nodes
         vector<Node*> nodes;
         for (Node* p = head; p; p = p->next)
             nodes.push_back(p);
-        
+
         int n = nodes.size();
-        // 2) reverse each chunk of size k, even the last partial chunk
-        for (int start = 0; start < n; start += k) {
+
+        for (int start=0;start<n;start+=k) {
             int end = min(start + k, n);
             reverse(nodes.begin() + start, nodes.begin() + end);
         }
         
-        // 3) relink in the new order
-        for (int i = 0; i + 1 < n; i++)
+
+        for (int i=0;i+1<n;i++)
             nodes[i]->next = nodes[i + 1];
-        nodes[n - 1]->next = nullptr;
+        nodes[n - 1]->next=nullptr;
         
-        // 4) new head is the first element in our vector
+
         return nodes[0];
     }
 };
