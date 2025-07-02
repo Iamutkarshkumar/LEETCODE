c067cf7c1383bf91979c9d600754dd87204f4a7d
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-07-02 10:17:42 +0530

    Time: 3 ms (66.70%) | Memory: 7.8 MB (36.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     int fib(int n) {
         if(n<=1) return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        int a=0,b=1;
+        int ans;
+        for(int i=2;i<=n;i++){
+            ans=a+b;
+            a=b;
+            b=ans;
+        }
+        return ans;
     }
 };
 //METHOD 2
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<=1) return n;
+//         vector<int> dp(n+1,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+//         return dp[n];
+//     }
+// };
+//METHOD 3
 // class Solution {
 // public:
 //     int solve(int n,vector<int>& dp){
