9da604a2caf3a22ae0cade7a053bfc562b6d3bf0
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-07-13 10:37:41 +0530

    Time: 26 ms (30.88%), Space: 48.4 MB (25.22%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int i){
+        if(parent[i]==i) return i;
+        return parent[i]=find(parent[i]);
+    }
+
+    void Union(int x,int y){
+        int par_x=find(x);
+        int par_y=find(y);
+
+        if(rank[par_x]>rank[par_y]) parent[par_y]=par_x;
+        else if(rank[par_x]<rank[par_y]) parent[par_x]=par_y;
+        else{
+            parent[par_y]=par_x;
+            rank[par_x]++;
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n-1) return -1;
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0;i<n;i++){
+            rank[i]=0;
+            parent[i]=i;
+        }
+
+        for(auto ele: connections){
+            if(find(ele[0])!=find(ele[1])) Union(ele[0],ele[1]);
+        }
+        unordered_set<int> s;
+        for(int i=0;i<n;i++) s.insert(find(i));
+        return s.size()-1;
+
+    }
+};
