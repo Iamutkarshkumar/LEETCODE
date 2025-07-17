d8106f96f4a0b7042f1d16de8a77b5ddf4e28ff3
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-07-17 09:33:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,27 +1,56 @@
 class Solution {
   public:
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfs(vector<vector<int>> &adj) {
-        int n=adj.size();
-        vector<int> visited(n,0);
+    void BFS(vector<vector<int>> &adj,int u,vector<int>& result,vector<bool>& visited){
         queue<int> qu;
-        vector<int> ans;
-        qu.push(0);
-        visited[0]=1;
+        qu.push(u);
+        visited[u]=true;
+        result.push_back(u);
         while(!qu.empty()){
             int u=qu.front();
             qu.pop();
-            ans.push_back(u);
-            
-            for(auto ele: adj[u]) {
-                if(! visited[ele]){
-                    visited[ele]=1;
-                    qu.push(ele);
+            for(int v: adj[u]){
+                if(!visited[v]){
+                    qu.push(v);
+                    visited[v]=true;
+                    result.push_back(v);
                 }
-                
             }
         }
-        return ans;
+    }
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int V=adj.size();
+        vector<int> result;
+        vector<bool> visited(V,false);
+        BFS(adj,0,result,visited);
+        return result;
         
     }
 };
+
+// class Solution {
+//   public:
+//     // Function to return Breadth First Traversal of given graph.
+//     vector<int> bfs(vector<vector<int>> &adj) {
+//         int n=adj.size();
+//         vector<bool> visited(n,false);
+//         queue<int> qu;
+//         vector<int> ans;
+//         qu.push(0);
+//         visited[0]=true;
+//         while(!qu.empty()){
+//             int u=qu.front();
+//             qu.pop();
+//             ans.push_back(u);
+            
+//             for(auto ele: adj[u]) {
+//                 if(! visited[ele]){
+//                     visited[ele]=true;
+//                     qu.push(ele);
+//                 }
+                
+//             }
+//         }
+//         return ans;
+        
+//     }
+// };
