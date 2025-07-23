32bf40fce739cd21670f24fb64ab79d24ab43c42
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-07-23 09:20:46 +0530

    Time: 4 ms (30.62%), Space: 21.9 MB (7.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>>& adj,int u,vector<int>& visited){
+    void bfs(unordered_map<int,vector<int>>& adj,int u,vector<int>& visited){
+        queue<int> que;
+        que.push(u);
         visited[u]=true;
-        for(auto &v: adj[u]){
-            if(!visited[v]){
-                dfs(adj,v,visited);
+
+        while(!que.empty()){
+            int u=que.front();
+            que.pop();
+            for(auto &v: adj[u]){
+                if(!visited[v]){
+                    que.push(v);
+                    visited[v]=true;
+                }
             }
         }
     }
@@ -27,9 +35,44 @@ public:
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 count++;
-                dfs(adj,i,visited);
+                bfs(adj,i,visited);
             }
         }
         return count;
     }
 };
+// class Solution {
+// public:
+//     void dfs(unordered_map<int,vector<int>>& adj,int u,vector<int>& visited){
+//         visited[u]=true;
+//         for(auto &v: adj[u]){
+//             if(!visited[v]){
+//                 dfs(adj,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n=isConnected.size();
+//         unordered_map<int,vector<int>> adj;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j]){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+
+//         vector<int> visited(n,0);
+//         int count=0;
+
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 count++;
+//                 dfs(adj,i,visited);
+//             }
+//         }
+//         return count;
+//     }
+// };
