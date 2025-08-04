5ed0bdf9dff137cf70ade3f32587eb1fe8c6b804
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-04 21:52:02 +0530

    Time: 9 ms (9.21%), Space: 11 MB (63.45%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,23 +1,70 @@
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         string ans="";
+//         unordered_map<char,int> mp;
+//         for(auto ele: s) mp[ele]++;
+//         priority_queue<pair<int,char>> pq;
+//         for(auto ele: mp){
+//             char first=ele.first;
+//             int second=ele.second;
+//             pq.push({second,first});
+//         }
+//         while(!pq.empty()){
+//             auto ele =pq.top();
+//             pq.pop();
+//             int freq=ele.first;
+//             char c=ele.second;
+//             while(freq--) ans+=c;
+//         }
+//         return ans;
+        
+//     }
+// };
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         string ans="";
+//         unordered_map<char,int> mp;
+//         for(auto ele: s) mp[ele]++;
+//         priority_queue<pair<int,char>> pq;
+//         for(auto ele: mp){
+//             char first=ele.first;
+//             int second=ele.second;
+//             pq.push({second,first});
+//         }
+//         while(!pq.empty()){
+//             auto ele =pq.top();
+//             pq.pop();
+//             int freq=ele.first;
+//             char c=ele.second;
+//             while(freq--) ans+=c;
+//         }
+//         return ans;
+        
+//     }
+// };
 class Solution {
 public:
+    static bool cmp(pair<int,char> a , pair<int,char> b){
+        return a.first>b.first;
+    }
     string frequencySort(string s) {
-        string ans="";
         unordered_map<char,int> mp;
         for(auto ele: s) mp[ele]++;
-        priority_queue<pair<int,char>> pq;
+        vector<pair<int,char>> v;
         for(auto ele: mp){
-            char first=ele.first;
-            int second=ele.second;
-            pq.push({second,first});
+            v.push_back({ele.second,ele.first});
         }
-        while(!pq.empty()){
-            auto ele =pq.top();
-            pq.pop();
-            int freq=ele.first;
-            char c=ele.second;
-            while(freq--) ans+=c;
+        sort(v.begin(),v.end(),cmp);
+        string ans;
+        for(auto ele: v){
+            int c=ele.first;
+            char x=ele.second;
+            while(c--){
+                ans.push_back(x);
+            }
         }
         return ans;
-        
     }
 };
