ab2295539848d389236ab23bd87e39488572df7c
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-06 19:12:04 +0530

    Time: 455 ms (28.5%), Space: 41.7 MB (40.22%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[2501][2501];
+    int solve(int i, int p ,vector<int>& nums){
+        if(i>=nums.size()) return 0;
+        if(p!=-1 and dp[i][p]!=-1) return dp[i][p];
+        //take
+        int take=0;
+        if(p==-1 or nums[p]<nums[i]){
+            take = 1+solve(i+1,i,nums);
+        }
+
+        // skip
+        int skip = solve(i+1,p,nums);
+        if(p!=-1) dp[i][p]=max(take,skip);
+        return max(take,skip);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,-1,nums);
+    }
+};
