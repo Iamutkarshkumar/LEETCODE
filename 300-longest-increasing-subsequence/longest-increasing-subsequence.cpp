5b37256890b20b9e19da2e85c954cb9ee6bfa69c
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-08 16:47:17 +0530

    Time: 5 ms (78.43%) | Memory: 13.9 MB (94.59%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n= nums.size();
-        vector<int> dp(n,1);
-        int ans=1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
-                    ans=max(ans,dp[i]);
-                }
-            }
+        //using patience sorting
+
+        vector<int> ps;
+        ps.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            int idx=0;
+            while(idx<ps.size() && ps[idx] < nums[i]) idx++; // find position to replace
+            if(idx < ps.size()) ps[idx] = nums[i]; // replace pile top
+            else ps.push_back(nums[i]); // new pile
         }
-        return ans;
+        return ps.size();
     }
 };
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<int> dp(n,1);
+//         int ans=1;
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[j]<nums[i]){
+//                     dp[i] = max(dp[i],dp[j]+1);
+//                     ans=max(ans,dp[i]);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 // class Solution {
 // public:
 //     int dp[2501][2501];
