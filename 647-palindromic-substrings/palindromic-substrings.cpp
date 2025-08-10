3c315a9b06010b0e238178dbcbc05272fea53d09
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-10 09:56:29 +0530

    Time: 1132 ms (11.50%) | Memory: 464.5 MB (5.16%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    unordered_map<string,int> dp;
     bool checkPalindrome(int i, int j,string& s){
         int len=j-i+1;
         string x = s.substr(i,len);
-        if(dp.count(x)) return dp[x];
-        for(int i=0;i<len/2;i++) if(x[i]!=x[len-i-1]) return dp[x]=false;
-        return dp[x]=true;
+        for(int i=0;i<len/2;i++) if(x[i]!=x[len-i-1]) return false;
+        return true;
     }
     int countSubstrings(string s) {
         int n=s.length();
