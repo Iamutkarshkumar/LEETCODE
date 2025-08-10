af7fc4e97b7afc87815dd19f1736bb5c77da3270
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-10 10:04:22 +0530

    Time: 27 ms (36.49%) | Memory: 13.1 MB (42.10%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    int dp[1001][1001];
     bool checkPalindrome(int i, int j,string& s){
-        int len=j-i+1;
-        string x = s.substr(i,len);
-        for(int i=0;i<len/2;i++) if(x[i]!=x[len-i-1]) return false;
-        return true;
+        if(i>=j) return dp[i][j]=true;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j]) return dp[i][j]=checkPalindrome(i+1,j-1,s);
+        return dp[i][j]=false;
     }
     int countSubstrings(string s) {
         int n=s.length();
+        memset(dp,-1,sizeof(dp));
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
