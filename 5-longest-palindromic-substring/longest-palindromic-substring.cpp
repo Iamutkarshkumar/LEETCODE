9495688657a3496ca38f850275a9524d587cdb5e
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-13 12:29:21 +0530

    Time: 323 ms (17.81%) | Memory: 26 MB (38.75%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool isPalindrome(int i,int j,string& s){
-        if(i >= j) return dp[i][j] = true;        
-        if(dp[i][j] != -1) return dp[i][j];       
-        if(s[i] != s[j]) return dp[i][j] = false; 
-        return dp[i][j] = isPalindrome(i+1, j-1, s); 
-    }
     string longestPalindrome(string s) {
-        memset(dp,-1,sizeof(dp));
         int n=s.length();
-        priority_queue<pair<int,pair<int,int>>> q;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(i,j,s)) q.push({j-i+1 , {i,j}});
+        int startingPoint=0;
+        int maxLen=1;
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        for(int L=1;L<=n;L++){//blue print codestorywithmik
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+                if(i==j) dp[i][i]=true;
+                else if(i+1==j) dp[i][j]=(s[i]==s[i+1]);
+                else dp[i][j]=(s[i]==s[j] and dp[i+1][j-1]);
+            
+                if (dp[i][j] && L > maxLen) {
+                    maxLen = L;
+                    startingPoint = i;
+                }
             }
         }
-        auto x=q.top();
-        return s.substr(x.second.first,x.first);
+        return s.substr(startingPoint,maxLen);
     }
 };
+// class Solution {
+// public:
+//     int dp[1001][1001];
+//     bool isPalindrome(int i,int j,string& s){
+//         if(i >= j) return dp[i][j] = true;        
+//         if(dp[i][j] != -1) return dp[i][j];       
+//         if(s[i] != s[j]) return dp[i][j] = false; 
+//         return dp[i][j] = isPalindrome(i+1, j-1, s); 
+//     }
+//     string longestPalindrome(string s) {
+//         memset(dp,-1,sizeof(dp));
+//         int n=s.length();
+//         int startingPoint=0;
+//         int maxLen=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(isPalindrome(i,j,s)){
+//                     int l=j-i+1;
+//                     if(l>maxLen) {maxLen=l;startingPoint=i;}
+//                 }
+//             }
+//         }
+//         return s.substr(startingPoint,maxLen);
+//     }
+// };
