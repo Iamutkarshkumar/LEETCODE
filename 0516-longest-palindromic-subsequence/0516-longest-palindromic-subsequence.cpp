213d49706b128815485be781a134cb5994c7352c
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-13 18:35:36 +0530

    Time: 63 ms (67%), Space: 75.9 MB (63.09%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(int i,int j,string& s){
-        if(i>j) return 0;
-        if(i==j) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j]) return dp[i][j]=2+solve(i+1,j-1,s);
-
-        int take_i_skip_j=solve(i,j-1,s);
-        int take_j_skip_i=solve(i+1,j,s);
-        return dp[i][j]= max(take_i_skip_j,take_j_skip_i);
-    }
     int longestPalindromeSubseq(string s) {
-        memset(dp,-1,sizeof(dp));
         int n=s.length();
-        return solve(0,n-1,s);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++) dp[i][i]=1;
+        for(int L=2;L<=n;L++){
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+                if(s[i]==s[j]) dp[i][j] = 2+dp[i+1][j-1];
+                else dp[i][j] = max(dp[i][j-1] , dp[i+1][j]);
+            }
+        }
+        return dp[0][n-1];
     }
 };
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s) {
+//         int n=s.length();
+//         vector<vector<int>> dp(n,vector<int>(n,0));
+//         for(int L=1;L<=n;L++){
+//             for(int i=0;i+L-1<n;i++){
+//                 int j=i+L-1;
+//                 if(i==j) dp[i][i]=1;
+//                 else if(s[i]==s[j]) dp[i][j] = 2+dp[i+1][j-1];
+//                 else dp[i][j] = max(dp[i][j-1] , dp[i+1][j]);
+//             }
+//         }
+//         return dp[0][n-1];
+//     }
+// };
+// class Solution {
+// public:
+//     int dp[1001][1001];
+//     int solve(int i,int j,string& s){
+//         if(i>j) return 0;
+//         if(i==j) return 1;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(s[i]==s[j]) return dp[i][j]=2+solve(i+1,j-1,s);
+
+//         int take_i_skip_j=solve(i,j-1,s);
+//         int take_j_skip_i=solve(i+1,j,s);
+//         return dp[i][j]= max(take_i_skip_j,take_j_skip_i);
+//     }
+//     int longestPalindromeSubseq(string s) {
+//         memset(dp,-1,sizeof(dp));
+//         int n=s.length();
+//         return solve(0,n-1,s);
+//     }
+// };
