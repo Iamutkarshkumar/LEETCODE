577bd775e259aa2399ed7d68266cea97ea6da6a3
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-08-18 07:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (91.49%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,35 @@
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m=grid.size();
+//         int n=grid[0].size();
+//         vector<vector<int>> dp(m,vector<int>(n));
+//         dp[0][0]=grid[0][0];
+//         for(int col=1;col<n;col++) dp[0][col]=grid[0][col]+dp[0][col-1];
+//         for(int row=1;row<m;row++) dp[row][0]=grid[row][0]+dp[row-1][0];
+//         for(int i=1;i<m;i++){
+//             for(int j=1;j<n;j++){
+//                 dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
 class Solution {
 public:
+    int m,n;
+    int dp[201][201];
+    int solve(int i,int j,vector<vector<int>>& grid){
+        if(i==m-1 and j==n-1) return grid[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==m-1) return  dp[i][j]= grid[i][j]+solve(i,j+1,grid);
+        if(j==n-1) return  dp[i][j]= grid[i][j]+solve(i+1,j,grid);
+        else return  dp[i][j]=grid[i][j]+min(solve(i+1,j,grid),solve(i,j+1,grid));
+    }
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n));
-        dp[0][0]=grid[0][0];
-        for(int col=1;col<n;col++) dp[0][col]=grid[0][col]+dp[0][col-1];
-        for(int row=1;row<m;row++) dp[row][0]=grid[row][0]+dp[row-1][0];
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[m-1][n-1];
+        memset(dp,-1,sizeof(dp));
+        m=grid.size();
+        n=grid[0].size();
+        return solve(0,0,grid);
     }
 };
