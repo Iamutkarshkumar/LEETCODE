f679425bbc7b481ff50bff658592d5ac910edc9f
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-09-09 12:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (96.05%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
-
-        // Count "breaks" in the sorted order
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > nums[(i + 1) % n]) {
-                count++;
-            }
-            // If there is more than one break, return false
-            if (count > 1) return false;
+        int n=nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i]>nums[(i+1)%n]) ans++;
         }
-
-        return true;
+        return ans<=1;
     }
 };
