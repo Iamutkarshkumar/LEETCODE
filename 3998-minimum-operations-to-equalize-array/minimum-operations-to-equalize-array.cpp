32fa43179e9fbb341f29dac27e920e9456d17f4f
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-09-15 22:20:06 +0530

    Time: 1 ms (14.94%) | Memory: 31.5 MB (12.23%) - LeetSync

--- 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.cpp
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.cpp
@@ -1,9 +1,16 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& n) {
+//         for(int i=1;i<n.size();i++){
+//             if(n[i]!=n[i-1]) return 1;
+//         }
+//         return 0;
+//     }
+// };
 class Solution {
 public:
-    int minOperations(vector<int>& n) {
-        for(int i=1;i<n.size();i++){
-            if(n[i]!=n[i-1]) return 1;
-        }
-        return 0;
+    int minOperations(vector<int>& nums) {
+        unordered_set<int> s(begin(nums),end(nums));
+        return s.size()!=1;
     }
 };
