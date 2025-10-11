5491f04104f1c21b93029e6412f6c80e20f7b352
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-11 18:12:27 +0530

    Time: 446 ms (23.92%), Space: 250.3 MB (13.52%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -1,33 +1,24 @@
+using ll=long long;
 class Solution {
 public:
+    unordered_map<ll,ll> mp;
+    int n;
+    vector<ll> dp;
+    ll solve(int i,vector<ll>& v){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        ll skip=solve(i+1,v);
+        int j=lower_bound(begin(v)+i+1,end(v),v[i]+3)-begin(v);
+        ll take=mp[v[i]]*v[i] +solve(j,v);
+        return dp[i]=max(take,skip);
+    }
     long long maximumTotalDamage(vector<int>& power) {
-        if(power.empty()) return 0;
-        unordered_map<long long,long long> freq;
-        for(int x: power) freq[x] += 1;
-
-        // collect unique values sorted
-        vector<long long> vals;
-        vals.reserve(freq.size());
-        for(auto &p: freq) vals.push_back(p.first);
-        sort(vals.begin(), vals.end());
-
-        int n = vals.size();
-        vector<long long> sum(n);   // sum[i] = vals[i] * count(vals[i])
-        for(int i=0;i<n;i++) sum[i] = vals[i] * freq[vals[i]];
-
-        vector<long long> dp(n,0);
-        for(int i=0;i<n;i++){
-            // find prev index j such that vals[j] <= vals[i]-3
-            long long need = vals[i] - 3; // allowed difference > 2 => prev <= vals[i]-3
-            int j = -1;
-            if(need >= vals[0]) {
-                auto it = upper_bound(vals.begin(), vals.begin() + i, need);
-                if(it != vals.begin()) j = int(it - vals.begin()) - 1;
-            }
-            long long take = sum[i] + (j >= 0 ? dp[j] : 0);
-            long long skip = (i > 0 ? dp[i-1] : 0);
-            dp[i] = max(take, skip);
-        }
-        return dp[n-1];
+        for(auto ele: power) mp[ele]++;
+        vector<ll> v(mp.size());
+        for(auto ele: mp) v.push_back(ele.first);
+        sort(begin(v),end(v));
+        n=v.size();
+        dp.assign(n+1,-1);
+        return solve(0,v); 
     }
 };
