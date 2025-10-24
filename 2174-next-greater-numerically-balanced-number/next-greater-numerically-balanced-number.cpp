a775a713081404529d3dcac6b6810a8266953895
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-24 08:56:54 +0530

    Time: 886 ms (14.57%) | Memory: 257.2 MB (9.05%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool help(int i){
+        vector<int> v(10);
+        string s=to_string(i);
+        for(int idx=0;idx<s.length();idx++){
+            v[s[idx]-'0']++;
+        }
+        for(int j=0;j<10;j++){
+            if(v[j]>0 and v[j]!=j) return false;
+        }
+        return true;
+    }
+    int nextBeautifulNumber(int n) {
+        for(int i=n+1;i<=1224444;i++){
+            if(help(i)) return i;
+        }
+        return 1;
+    }
+};
