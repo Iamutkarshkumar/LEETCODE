32d534936b6ef9efe77447dfa9dc8a8ce350ebdd
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-25 07:48:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.24%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        vector<int> v(n);
-        int prev=0,curr=1,count=0;
-        for(int i=0;i<n;i++){
-            if(i%7==0){
-                count=prev+1;
-                prev++;
-            }
-            v[i]=count;
-            count++;
+        int fullWeeks=n/7;
+        int leftDays=n%7;
+        int x=28;
+        int y=fullWeeks+1;
+         long long ans = 0;
+        // sum of AP: fullWeeks * (2*a + (fullWeeks-1)*d) / 2
+        // a = 28, d = 7
+        ans = fullWeeks * (56 + (fullWeeks - 1) * 7LL) / 2LL;
+        // while(fullWeeks--){
+        //     ans+=x;
+        //     x+=7;
+        // }
+        while(leftDays--){
+            ans+=y;
+            y++;
         }
-        int ans=0;
-        for(auto ele: v) ans+=ele;
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         vector<int> v(n);
+//         int prev=0,curr=1,count=0;
+//         for(int i=0;i<n;i++){
+//             if(i%7==0){
+//                 count=prev+1;
+//                 prev++;
+//             }
+//             v[i]=count;
+//             count++;
+//         }
+//         int ans=0;
+//         for(auto ele: v) ans+=ele;
+//         return ans;
+//     }
+// };
