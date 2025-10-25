2dbf46ed63cb0cc18c076fbda27a4534700dda72
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-25 07:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int fullWeeks=n/7;
+        int leftDays=n%7;
+        int x=28;
+        int y=fullWeeks+1;
+        int ans=0;
+        while(fullWeeks--){
+            ans+=x;
+            x+=7;
+        }
+        while(leftDays--){
+            ans+=y;
+            y++;
+        }
+        return ans;
+    }
+};
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         vector<int> v(n);
+//         int prev=0,curr=1,count=0;
+//         for(int i=0;i<n;i++){
+//             if(i%7==0){
+//                 count=prev+1;
+//                 prev++;
+//             }
+//             v[i]=count;
+//             count++;
+//         }
+//         int ans=0;
+//         for(auto ele: v) ans+=ele;
+//         return ans;
+//     }
+// };
