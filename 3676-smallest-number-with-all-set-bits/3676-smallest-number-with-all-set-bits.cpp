816d42adf79f46ad891bd83b962e9d3f37e89a1a
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-29 08:30:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (60.8%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int count=0;
-        while(n){
-            count++;
-            n>>=1;
-        }
-        int ans=0;
-        while(count--){ 
-            ans<<=1;
-            ans=ans|1;
+        int ans=1;
+        int x=2;
+        while(ans<n){
+            ans+=x;
+            x*=2;
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int smallestNumber(int n) {
+//         int count=0;
+//         while(n){
+//             count++;
+//             n>>=1;
+//         }
+//         int ans=0;
+//         while(count--){ 
+//             ans<<=1;
+//             ans=ans|1;
+//         }
+//         return ans;
+//     }
+// };
