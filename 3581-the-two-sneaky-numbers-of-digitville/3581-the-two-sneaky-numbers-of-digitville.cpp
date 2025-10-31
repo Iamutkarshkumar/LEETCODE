2bc097d432bffeb95e19717f24e8a26cc6e72906
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-10-31 08:42:22 +0530

    Time: 16 ms (9.16%), Space: 25.1 MB (95.85%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -3,11 +3,21 @@ public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans;
-        vector<int> freq(n,0);
-        for(int i: nums) freq[i]++;
-        for(int i=0;i<n;i++){
-            if(freq[i]==2) ans.push_back(i);
-        }
+        sort(begin(nums),end(nums));
+        for(int i=1;i<n;i++) if(nums[i]==nums[i-1]) ans.push_back(nums[i]);
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> ans;
+//         vector<int> freq(n,0);
+//         for(int i: nums) freq[i]++;
+//         for(int i=0;i<n;i++){
+//             if(freq[i]==2) ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
