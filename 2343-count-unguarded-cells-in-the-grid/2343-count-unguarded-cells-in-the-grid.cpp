c88eb7800249f15315a58f292b41bd4578ceb758
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-11-02 08:28:47 +0530

    Time: 210 ms (22.48%), Space: 209.1 MB (25.23%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         vector<vector<int>> ans(m,vector<int>(n,0));
-        for(auto ele: guards) ans[ele[0]][ele[1]]=2;
-        for(auto ele: walls) ans[ele[0]][ele[1]]=3;
+        for(auto ele: guards) ans[ele[0]][ele[1]]=-1;
+        for(auto ele: walls) ans[ele[0]][ele[1]]=-1;
 
         for(auto ele: guards){
             int r=ele[0];
             int c=ele[1];
             //up
             for(int i=r-1;i>=0;i--){
-                if(ans[i][c]==2 or ans[i][c]==3) break;
+                if(ans[i][c]==-1) break;
                 ans[i][c]=1;
             }
             //down
             for(int i=r+1;i<m;i++){
-                if(ans[i][c]==2 or ans[i][c]==3) break;
+                if(ans[i][c]==-1) break;
                 ans[i][c]=1;
             }
             //left
             for(int i=c-1;i>=0;i--){
-                if(ans[r][i]==2 or ans[r][i]==3) break;
+                if(ans[r][i]==-1) break;
                 ans[r][i]=1;
             }
             //right
             for(int i=c+1;i<n;i++){
-                if(ans[r][i]==2 or ans[r][i]==3) break;
+                if(ans[r][i]==-1) break;
                 ans[r][i]=1;
             }
         }
