2633eb1d34e22c1034a78569e885c973e16411d4
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2025-12-28 17:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (67.95%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int neg(vector<int>& v){
-        int n=v.size();
-        int l=0,h=n-1;
-        int idx=n;
-        while(l<=h){
-            int m=l+(h-l)/2;
-            if(v[m]<0){
-                idx=m;
-                h=m-1;
-            }
-            else l=m+1;
-        }
-        return idx;
-    }
-    int countNegatives(vector<vector<int>>& grid) {
-        int l=grid[0].size();
+    int countNegatives(vector<vector<int>>& v) {
+        int m=v.size();
+        int n=v[0].size();
         int ans=0;
-        for(auto ele: grid){
-            int idx=neg(ele);
-            ans+=(l-idx);
+        for(int i=0;i<m;i++){
+            int idx=upper_bound(v[i].begin(),v[i].end(),0,greater<int>())-v[i].begin();
+            ans+=abs(n-idx);
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int neg(vector<int>& v){
+//         int n=v.size();
+//         int l=0,h=n-1;
+//         int idx=n;
+//         while(l<=h){
+//             int m=l+(h-l)/2;
+//             if(v[m]<0){
+//                 idx=m;
+//                 h=m-1;
+//             }
+//             else l=m+1;
+//         }
+//         return idx;
+//     }
+//     int countNegatives(vector<vector<int>>& grid) {
+//         int l=grid[0].size();
+//         int ans=0;
+//         for(auto ele: grid){
+//             int idx=neg(ele);
+//             ans+=(l-idx);
+//         }
+//         return ans;
+//     }
+// };
