814f984f30f1abfa3d17697182eaf78151a20dab
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-02 13:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (85.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,12 @@
 class Solution {
 public:
-//using in-built functions
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int l=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int r=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(l==r) return {-1,-1};
-        return {l,r-1};
+    vector<int> searchRange(vector<int>& n, int t) {
+        int start=-1,end=-1;
+        if(binary_search(n.begin(),n.end(),t)){
+            start=lower_bound(n.begin(),n.end(),t)-begin(n);
+            end=upper_bound(n.begin(),n.end(),t)-begin(n)-1;
+        }
+        
+        return {start,end};
     }
 };
-// class Solution {
-// public:
-//     int LB(vector<int>& nums, int target){
-//         int n=nums.size();
-//         int l=0,r=n-1,ans=n;
-//         while(l<=r){
-//             int m = l+(r-l)/2;
-//             if(nums[m]>=target){
-//                 ans=min(ans,m);
-//                 r=m-1;
-//             }
-//             else l=m+1;
-//         }
-//         return ans;
-//     }
-//     int UB(vector<int>& nums, int target){
-//         int n=nums.size();
-//         int l=0,r=n-1,ans=n;
-//         while(l<=r){
-//             int m = l+(r-l)/2;
-//             if(nums[m]>target){
-//                 ans=min(ans,m);
-//                 r=m-1;
-//             }
-//             else l=m+1;
-//         }
-//         return ans;
-//     }
-
-//     vector<int> searchRange(vector<int>& nums, int target) {
-//         int l = LB(nums, target);
-//         if(l == nums.size() || nums[l] != target)
-//             return {-1, -1};  // target not found
-//         int r = UB(nums, target);
-//         return {l, r - 1};
-//     }
-// };
