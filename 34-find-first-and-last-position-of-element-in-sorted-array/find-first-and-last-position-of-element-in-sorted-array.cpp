0aec5eb6e41ae30585c210fff6a2a2e347ac0023
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-02 13:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (16.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,43 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& n, int t) {
+    vector<int> searchRange(vector<int>& v, int t) {
+        int n=v.size();
+        if(n==0) return {-1,-1};
+
+        int l=0,h=n-1;
         int start=-1,end=-1;
-        if(binary_search(n.begin(),n.end(),t)){
-            start=lower_bound(n.begin(),n.end(),t)-begin(n);
-            end=upper_bound(n.begin(),n.end(),t)-begin(n)-1;
+
+        // lower bound (first occurrence)
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(v[m]>=t) h=m-1;
+            else l=m+1;
         }
-        
+        if(l<n && v[l]==t) start=l;
+
+        // upper bound (last occurrence)
+        l=0; h=n-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(v[m]<=t){
+                if(v[m]==t) end=m;
+                l=m+1;
+            } 
+            else h=m-1;
+        }
+
         return {start,end};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& v, int t) {
+//         int start=-1,end=-1;
+//         if(binary_search(v.begin(),v.end(),t)){
+//             start=lower_bound(v.begin(),v.end(),t)-begin(v);
+//             end=upper_bound(v.begin(),v.end(),t)-begin(v)-1;
+//         }
+//         return {start,end};
+//     }
+// };
