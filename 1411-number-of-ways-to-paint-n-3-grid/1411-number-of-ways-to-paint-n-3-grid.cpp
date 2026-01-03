14d039a58e8b6c1b4ec9a31c2076de96962a3240
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-03 09:54:34 +0530

    Time: 263 ms (15.67%), Space: 91.9 MB (9.33%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve(int i,int n,int prev1,int prev2,int prev3,int mod,vector<vector<vector<vector<int>>>>& dp){
+        if(i==n) return 1;
+        if(dp[i][prev1][prev2][prev3]!=-1) return dp[i][prev1][prev2][prev3];
+        int ans=0;
+        for(int c1=1;c1<=3;c1++){
+            if(c1==prev1) continue;
+            for(int c2=1;c2<=3;c2++){
+                if(c2==c1 or c2==prev2) continue;
+                for(int c3=1;c3<=3;c3++){
+                    if(c3==c2 or c3==prev3) continue;
+                    ans = (ans + solve(i+1,n,c1,c2,c3,mod,dp))%mod;
+                }
+            }
+        }
+        return dp[i][prev1][prev2][prev3]=ans;
+            
+        
+    }
+    int numOfWays(int n) {
+        vector<vector<vector<vector<int>>>> dp(
+            n,
+            vector<vector<vector<int>>>(
+                4,
+                vector<vector<int>>(
+                    4,
+                    vector<int>(4, -1)
+                )
+            )
+        );
+
+        int mod=1e9+7;
+        return solve(0,n,0,0,0,mod,dp);
+    }
+};
