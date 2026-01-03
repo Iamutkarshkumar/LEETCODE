1d39479240fbaf215027abab6bdb60e4e98f22ce
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-03 12:21:59 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.32%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int l=0,r=n-1;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(nums[m]==target) return true;
-            if(nums[l]==nums[m] and nums[m]==nums[r]){
-                l++;r--;
+    bool BS(vector<int>& nums,int low,int high,int t){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==t){
+                return true;
             }
-            //left half sorted 
-            else if(nums[l]<=nums[m]){
-                if(nums[l]<=target and nums[m]>target) r=m-1;
-                else l=m+1;
-            }
-            //right half sorted
-            else{
-                if(nums[m]<target and nums[r]>=target) l=m+1;
-                else r=m-1;
-            }
+            else if(nums[mid]>t) high=mid-1;
+            else low=mid+1;
         }
         return false;
+    }
+    int findPivot(vector<int>& nums){
+        int n=nums.size();
+        int l=0,r=n-1;
+        while(l<r){
+            while(l<r and nums[l]==nums[l+1]) l++;
+            while(l<r and nums[r]==nums[r-1]) r--;
+            int m=l+(r-l)/2;
+            if(nums[m]>nums[r]) l=m+1;
+            else r=m;
+        }
+        return l;
+    }
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int pivot=findPivot(nums);
+        if(BS(nums,0,pivot-1,target)) return true;
+        return BS(nums,pivot,n-1,target);
     }
 };
