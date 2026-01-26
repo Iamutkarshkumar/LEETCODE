e93dfb53049f2328bdcf3b92ded883b72e376eb3
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-26 10:22:56 +0530

    Time: 15 ms (76.77%), Space: 36.7 MB (32.02%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,14 +4,12 @@ public:
         int n=arr.size();
         int minEle=INT_MAX;
         sort(begin(arr),end(arr));
-        vector<int> v(n-1);
         for(int i=0;i<n-1;i++){
-            v[i]=abs(arr[i]-arr[i+1]);
-            minEle=min(minEle,v[i]);
+            minEle=min(minEle,abs(arr[i]-arr[i+1]));
         } 
         vector<vector<int>> ans;
         for(int i=0;i<n-1;i++){
-            if(v[i]==minEle) ans.push_back({arr[i],arr[i+1]});
+            if(abs(arr[i]-arr[i+1])==minEle) ans.push_back({arr[i],arr[i+1]});
         }
         return ans;
     }
