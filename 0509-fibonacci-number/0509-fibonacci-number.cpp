e8e1f8a08a6f95ac145dbacd24767dd3031f76c5
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-29 21:51:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (63.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -11,15 +11,32 @@
 //         return solve(n,dp);
 //     }
 // };
+
 //method 2: Bottom-up
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<=1) return n;
+//         vector<int> dp(n+1,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+//         return dp[n];
+//     }
+// };
+
+//method 3: constant SC
 class Solution {
 public:
     int fib(int n) {
         if(n<=1) return n;
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        int a=0,b=1;
+        int ans;
+        for(int i=2;i<=n;i++){
+            ans=a+b;
+            a=b;
+            b=ans;
+        }
+        return ans;
     }
 };
