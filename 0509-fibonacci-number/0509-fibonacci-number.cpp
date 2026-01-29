0fa51da69439ef3ab22f9833fd8f3b7701742001
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-01-29 21:48:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,25 @@
 // method 1: Top-down DP (Recursion + Memoization)
+// class Solution {
+// public:
+//     int solve(int n,vector<int>& dp){
+//         if(n<=1) return n; //base case
+//         if(dp[n]!=-1) return dp[n];
+//         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+//     }
+//     int fib(int n) {
+//         vector<int> dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+// };
+//method 2: Bottom-up
 class Solution {
 public:
-    int solve(int n,vector<int>& dp){
-        if(n<=1) return n; //base case
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
     int fib(int n) {
+        if(n<=1) return n;
         vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
