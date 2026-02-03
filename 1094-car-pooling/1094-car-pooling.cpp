38e9eb4faf237b590455de53cc73fdfbdf75831a
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-02-03 10:20:56 +0530

    Time: 0 ms (100%), Space: 14.7 MB (24.04%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,16 +1,34 @@
+//Approach-2 (Using Difference Array Technique)
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        map<int,int> mp;
+        vector<int> v(1001,0);
         for(auto ele: trips){
-            mp[ele[1]]+=ele[0];
-            mp[ele[2]]+=-1*ele[0];
+            v[ele[1]]+=ele[0];
+            v[ele[2]]-=ele[0];
         }
         int sum=0;
-        for(auto ele: mp){
-            sum+=ele.second;
+        for(int i=0;i<1001;i++){
+            sum+=v[i];
             if(sum>capacity) return false;
         }
         return true;
     }
 };
+//Approach-1 (Using Line Sweep)
+// class Solution {
+// public:
+//     bool carPooling(vector<vector<int>>& trips, int capacity) {
+//         map<int,int> mp;
+//         for(auto ele: trips){
+//             mp[ele[1]]+=ele[0];
+//             mp[ele[2]]+=-1*ele[0];
+//         }
+//         int sum=0;
+//         for(auto ele: mp){
+//             sum+=ele.second;
+//             if(sum>capacity) return false;
+//         }
+//         return true;
+//     }
+// };
