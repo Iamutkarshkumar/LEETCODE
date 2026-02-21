d4eb12d411e02905691e76fa39f6e74157fccbd8
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-02-21 07:41:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.78%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        int ans=0;
+        while(n){
+            if(n&1) ans++;
+            n>>=1;
+        }
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     int hammingWeight(int n) {
+//         return __builtin_popcount(n);
+//     }
+// };
