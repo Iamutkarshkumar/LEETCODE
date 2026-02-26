10503bba643aca69cba761dadc0215de1d135783
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-02-26 08:40:20 +0530

    Time: 0 ms (100%), Space: 74.1 MB (29.58%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,13 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+//         vector<int> ans(n+1,0);
+//         for(auto ele: bookings){
+//             ans[ele[0]-1]+=ele[2];
+//             ans[ele[1]]-=ele[2];
+//         }
+//         for(int i=1;i<n;i++) ans[i]+=ans[i-1];
+//         ans.pop_back();
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> ans(n+1,0);
-        for(auto ele: bookings){
-            ans[ele[0]-1]+=ele[2];
-            ans[ele[1]]-=ele[2];
+        vector<int> diff(n+2, 0);
+
+        for(auto &b : bookings) {
+            int start = b[0];
+            int end = b[1];
+            int count = b[2];
+
+            diff[start] += count;
+            diff[end+1] -= count;
         }
-        for(int i=1;i<n;i++) ans[i]+=ans[i-1];
-        ans.pop_back();
-        return ans;
+
+        //Cumulative sum
+        vector<int> result;
+        int cumSum = 0;
+        for(int i = 1; i <= n; i++) {
+            cumSum += diff[i];
+            result.push_back(cumSum);
+        }
+
+        return result;
     }
 };
