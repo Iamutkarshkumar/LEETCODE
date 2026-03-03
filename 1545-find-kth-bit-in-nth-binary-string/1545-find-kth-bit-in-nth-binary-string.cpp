bf7505dfabd4a1af33d3d457e5cb574536e43c9a
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-03-03 07:07:06 +0530

    Time: 95 ms (14.84%), Space: 98 MB (13.34%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,19 +1,40 @@
+// class Solution {
+// public:
+//     string help(string x){
+//         string ans="";
+//         for(int i=x.length()-1;i>=0;i--){
+//             x[i]=='1'? ans.push_back('0'):ans.push_back('1');
+//         }
+//         return ans;
+//     }
+//     char findKthBit(int n, int k) {
+//         vector<string> v(n);
+//         v[0]="0";
+//         for(int i=1;i<n;i++){
+//             string s=help(v[i-1]);
+//             v[i]=v[i-1]+"1"+s;
+//         }
+//         return v[n-1][k-1];
+//     }
+// };
 class Solution {
 public:
     string help(string x){
         string ans="";
-        for(int i=x.length()-1;i>=0;i--){
-            x[i]=='1'? ans.push_back('0'):ans.push_back('1');
-        }
+        for(int i=x.length()-1;i>=0;i--)
+            ans.push_back(x[i]=='1'?'0':'1');
         return ans;
     }
+
     char findKthBit(int n, int k) {
         vector<string> v(n);
         v[0]="0";
+
         for(int i=1;i<n;i++){
             string s=help(v[i-1]);
             v[i]=v[i-1]+"1"+s;
         }
+
         return v[n-1][k-1];
     }
 };
