e410ee7f84ba11b52be441313afc66ea68201a91
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-03-20 07:35:57 +0530

    Time: 16 ms (64.44%), Space: 31.9 MB (81.48%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -6,21 +6,22 @@ public:
         vector<vector<int>> ans(M,vector<int>(N,0));
         for(int i=0;i<M;i++){
             for(int j=0;j<N;j++){
-                priority_queue<int,vector<int>,greater<int>> pq;
-                unordered_set<int> s;
+                vector<int> v;
+                
                 for(int a=i;a<i+k;a++){
                     for(int b=j;b<j+k;b++){
-                        if(!s.count(grid[a][b])){
-                            pq.push(grid[a][b]);
-                            s.insert(grid[a][b]);
-                        }
+                        v.push_back(grid[a][b]);
                     }
                 }
+                
+                sort(v.begin(),v.end());
+                v.erase(unique(v.begin(),v.end()),v.end());
+                
                 int l=INT_MAX;
-                while(pq.size()>1){
-                    int x=pq.top();pq.pop();
-                    l=min(l,abs(x-pq.top()));
+                for(int x=1;x<v.size();x++){
+                    l=min(l,abs(v[x]-v[x-1]));
                 }
+                
                 if(l==INT_MAX) l=0;
                 ans[i][j]=l;
             }
@@ -28,3 +29,33 @@ public:
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+//         int m=grid.size(),n=grid[0].size();
+//         int M=m-k+1,N=n-k+1;
+//         vector<vector<int>> ans(M,vector<int>(N,0));
+//         for(int i=0;i<M;i++){
+//             for(int j=0;j<N;j++){
+//                 priority_queue<int,vector<int>,greater<int>> pq;
+//                 unordered_set<int> s;
+//                 for(int a=i;a<i+k;a++){
+//                     for(int b=j;b<j+k;b++){
+//                         if(!s.count(grid[a][b])){
+//                             pq.push(grid[a][b]);
+//                             s.insert(grid[a][b]);
+//                         }
+//                     }
+//                 }
+//                 int l=INT_MAX;
+//                 while(pq.size()>1){
+//                     int x=pq.top();pq.pop();
+//                     l=min(l,abs(x-pq.top()));
+//                 }
+//                 if(l==INT_MAX) l=0;
+//                 ans[i][j]=l;
+//             }
+//         }
+//         return ans;
+//     }
+// };
