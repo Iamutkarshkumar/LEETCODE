967e0ac209114b84443d4dcf230e1e34fc714eb7
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-03-22 16:22:58 +0530

    Time: 15 ms (26.82%), Space: 12.5 MB (79.75%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -6,24 +6,53 @@ public:
         int trueCount=0;
         while(j<n){
             if(answerKey[j]=='T') trueCount++;
-            if((j-i+1)-trueCount<=k) ans=max(ans,j-i+1);
-            else{
+            while((j-i+1)-trueCount>k){
                 if(answerKey[i]=='T') trueCount--;
                 i++;
             }
+            ans=max(ans,j-i+1);
             j++;
         }
         int falseCount=0;
         i=0,j=0;
         while(j<n){
-            if(answerKey[j]=='F') falseCount++;
-            if((j-i+1)-falseCount<=k) ans=max(ans,j-i+1);
-            else{
+           if(answerKey[j]=='F') falseCount++;
+            while((j-i+1)-falseCount>k){
                 if(answerKey[i]=='F') falseCount--;
                 i++;
             }
+            ans=max(ans,j-i+1);
             j++;
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     int maxConsecutiveAnswers(string answerKey, int k) {
+//         int n=answerKey.size(),i=0,j=0;
+//         int ans=0;
+//         int trueCount=0;
+//         while(j<n){
+//             if(answerKey[j]=='T') trueCount++;
+//             if((j-i+1)-trueCount<=k) ans=max(ans,j-i+1);
+//             else{
+//                 if(answerKey[i]=='T') trueCount--;
+//                 i++;
+//             }
+//             j++;
+//         }
+//         int falseCount=0;
+//         i=0,j=0;
+//         while(j<n){
+//             if(answerKey[j]=='F') falseCount++;
+//             if((j-i+1)-falseCount<=k) ans=max(ans,j-i+1);
+//             else{
+//                 if(answerKey[i]=='F') falseCount--;
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return ans;
+//     }
+// };
