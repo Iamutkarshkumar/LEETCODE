e8b3c597bf0e2fce64ac3c076982e4e58b3fa8d1
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-04-05 08:34:13 +0530

    Time: 4 ms (27.83%), Space: 10.4 MB (47.26%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int u=0,d=0,l=0,r=0;
+        int u=0,r=0;
         for(auto ele: moves){
             if(ele=='U') u++;
-            if(ele=='D') d++;
-            if(ele=='R') r++;
-            if(ele=='L') l++;
+            else if(ele=='D') u--;
+            else if(ele=='R') r++;
+            else if(ele=='L') r--;
         }
-        return (abs(u-d)+abs(r-l)==0);
+        return u==0 and r==0;
     }
 };
+// class Solution {
+// public:
+//     bool judgeCircle(string moves) {
+//         int u=0,d=0,l=0,r=0;
+//         for(auto ele: moves){
+//             if(ele=='U') u++;
+//             if(ele=='D') d++;
+//             if(ele=='R') r++;
+//             if(ele=='L') l++;
+//         }
+//         return (abs(u-d)+abs(r-l)==0);
+//     }
+// };
