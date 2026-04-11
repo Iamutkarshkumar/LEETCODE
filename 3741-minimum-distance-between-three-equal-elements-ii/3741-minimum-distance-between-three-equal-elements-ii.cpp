971dad2e56031438dd629b6485acfee79bf0e55d
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-04-11 07:37:41 +0530

    Time: 395 ms (54.75%), Space: 343.4 MB (65%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -4,13 +4,13 @@ public:
         int ans=INT_MAX;
         int n=nums.size();
         unordered_map<int,vector<int>> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]].push_back(i);
-            int x=mp[nums[i]].size();
+        for(int idx=0;idx<n;idx++){
+            mp[nums[idx]].push_back(idx);
+            int x=mp[nums[idx]].size();
             if(x>=3){
-                auto &v = mp[nums[i]];
-                int k=v[x-1];
-                ans=min(ans,2*(k-v[x-3]));
+                int k=mp[nums[idx]][x-1];
+                int i=mp[nums[idx]][x-3];
+                ans=min(ans,2*(k-i));
             }
         }
         return ans==INT_MAX ? -1 : ans;
