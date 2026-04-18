9388319201dabf9223e2f6fde0f5f190c1e6e7ba
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-04-18 22:09:02 +0530

    Time: 2 ms (6.74%), Space: 9.3 MB (8.47%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -1,14 +1,24 @@
+// class Solution {
+// public:
+//     int rev(int x){
+//         int y=0;
+//         while(x>0){
+//             y=y*10+x%10;
+//             x/=10;
+//         }
+//         return y;
+//     }
+//     int mirrorDistance(int n) {
+//         return abs(n-rev(n));
+//     }
+// };
 class Solution {
 public:
-    int rev(int x){
-        int y=0;
-        while(x>0){
-            y=y*10+x%10;
-            x/=10;
-        }
-        return y;
-    }
     int mirrorDistance(int n) {
-        return abs(n-rev(n));
+        string s = to_string(n);
+        reverse(begin(s), end(s));
+        
+        int rev = stoi(s);
+        return abs(n - rev);
     }
 };
