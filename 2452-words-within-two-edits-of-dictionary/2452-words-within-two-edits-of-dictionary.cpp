5659c79b67e33630016e799024a6b0b2b94a2b26
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-04-22 10:15:12 +0530

    Time: 0 ms (100%), Space: 12.5 MB (70.59%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int hamming(string& a,string& b){
+    bool isValid(string& a,string& b){
         int count=0;
         for(int i=0;i<a.length();i++){
             if(a[i]!=b[i]) count++;
-            if(count>2) return count;
+            if(count>2) return false;
         }
-        return count;
+        return true;
     }
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         int n=queries.size(),m=dictionary.size();
-        vector<int> hammDist;
         vector<string> ans;
         for(int i=0;i<n;i++){
-            int x=INT_MAX;
             for(int j=0;j<m;j++){
-                int dist=hamming(queries[i],dictionary[j]);
-                x=min(x,dist);
+                if(isValid(queries[i],dictionary[j])){
+                    ans.push_back(queries[i]);
+                    break;
+                }
             }
-            hammDist.push_back(x);
-        }
-        for(int i=0;i<n;i++){
-            if(hammDist[i]<=2) ans.push_back(queries[i]);
         }
         return ans;
     }
