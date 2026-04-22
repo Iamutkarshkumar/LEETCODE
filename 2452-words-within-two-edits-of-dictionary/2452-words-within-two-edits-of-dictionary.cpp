c9e53a35fa13253bf8124a1706685234f8e6369b
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-04-22 08:34:03 +0530

    Time: 15 ms (39.04%), Space: 12.5 MB (88.24%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int hamming(string& a,string& b){
+        int count=0;
+        for(int i=0;i<a.length();i++){
+            if(a[i]!=b[i]) count++;
+        }
+        return count;
+    }
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries.size(),m=dictionary.size();
+        vector<int> hammDist;
+        vector<string> ans;
+        for(int i=0;i<n;i++){
+            int x=INT_MAX;
+            for(int j=0;j<m;j++){
+                int dist=hamming(queries[i],dictionary[j]);
+                x=min(x,dist);
+            }
+            hammDist.push_back(x);
+        }
+        for(int i=0;i<n;i++){
+            if(hammDist[i]<=2) ans.push_back(queries[i]);
+        }
+        return ans;
+    }
+};
