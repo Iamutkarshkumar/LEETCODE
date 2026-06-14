8b3984df2e8f6c05b8949efca617c7507f42c602
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-14 17:37:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,56 +1,22 @@
 class Solution {
   public:
-    void BFS(vector<vector<int>> &adj,int u,vector<int>& result,vector<bool>& visited){
-        queue<int> qu;
-        qu.push(u);
-        visited[u]=true;
-        result.push_back(u);
-        while(!qu.empty()){
-            int u=qu.front();
-            qu.pop();
-            for(int v: adj[u]){
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int n=adj.size();
+        vector<bool> visited(n,false);
+        vector<int> result;
+        queue<int> q;
+        q.push(0);
+        visited[0]=true;
+        while(!q.empty()){
+            int u=q.front();q.pop();
+            result.push_back(u);
+            for(int &v: adj[u]){
                 if(!visited[v]){
-                    qu.push(v);
+                    q.push(v);
                     visited[v]=true;
-                    result.push_back(v);
                 }
             }
         }
-    }
-    vector<int> bfs(vector<vector<int>> &adj) {
-        int V=adj.size();
-        vector<int> result;
-        vector<bool> visited(V,false);
-        BFS(adj,0,result,visited);
         return result;
-        
     }
 };
-
-// class Solution {
-//   public:
-//     // Function to return Breadth First Traversal of given graph.
-//     vector<int> bfs(vector<vector<int>> &adj) {
-//         int n=adj.size();
-//         vector<bool> visited(n,false);
-//         queue<int> qu;
-//         vector<int> ans;
-//         qu.push(0);
-//         visited[0]=true;
-//         while(!qu.empty()){
-//             int u=qu.front();
-//             qu.pop();
-//             ans.push_back(u);
-            
-//             for(auto ele: adj[u]) {
-//                 if(! visited[ele]){
-//                     visited[ele]=true;
-//                     qu.push(ele);
-//                 }
-                
-//             }
-//         }
-//         return ans;
-        
-//     }
-// };
