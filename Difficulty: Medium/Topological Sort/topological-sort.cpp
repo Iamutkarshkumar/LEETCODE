7eb3b079ca5c3d2b2f043c597b9d01b7a15655be
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-21 12:37:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,29 +1,57 @@
+// class Solution {
+//   public:
+//     void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited,stack<int>& st){
+//         visited[u]=true;
+//         for(int v: adj[u]){
+//             if(!visited[v]) DFS(adj,v,visited,st);
+//         }
+//         st.push(u); // pehele mere baccho ko stack mein daalo uske baad stack mein daalo
+//         // when you return/backtrack then push that node to stack
+//     }
+//     vector<int> topoSort(int V, vector<vector<int>>& edges) {
+//         vector<vector<int>> adj(V);
+//         for(auto ele: edges) adj[ele[0]].push_back(ele[1]);
+        
+//         vector<bool> visited(V,false);
+//         stack<int> st;
+        
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]) DFS(adj,i,visited,st);
+//         }
+        
+//         vector<int> ans;
+//         while(!st.empty()){
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
   public:
-    void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited,stack<int>& st){
-        visited[u]=true;
-        for(int v: adj[u]){
-            if(!visited[v]) DFS(adj,v,visited,st);
-        }
-        st.push(u);
-    }
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         vector<vector<int>> adj(V);
         for(auto ele: edges) adj[ele[0]].push_back(ele[1]);
-        
-        vector<bool> visited(V,false);
-        stack<int> st;
-        
-        for(int i=0;i<V;i++){
-            if(!visited[i]) DFS(adj,i,visited,st);
+        vector<int> inDegree(V,0);
+        for(vector<int>& ele: adj){
+            for(auto node: ele){
+                inDegree[node]++;
+            }
+        }
+        queue<int> q;
+        for(int u=0;u<V;u++){
+            if(inDegree[u]==0) q.push(u); 
         }
         
         vector<int> ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        while(!q.empty()){
+            int u=q.front();q.pop();
+            ans.push_back(u);
+            for(int v: adj[u]){
+                inDegree[v]--;
+                if(inDegree[v]==0) q.push(v);
+            }
         }
         return ans;
-      
     }
 };
