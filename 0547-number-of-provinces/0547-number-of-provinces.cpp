eb807880395aa4ec66a2e971fe06ffcaa2ac714e
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-21 17:27:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (83.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited){
+    int n;
+    void DFS(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
         visited[u]=true;
-        for(int v: adj[u]){
-            if(!visited[v]) DFS(adj,v,visited);
+        for(int v=0;v<n;v++){
+            if(!visited[v] and isConnected[u][v]==1) DFS(isConnected,v,visited);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1) adj[i].push_back(j);
-            }
-        }
+        n=isConnected.size();
         vector<bool> visited(n,false);
         int numberOfTimesDfsCalled=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 numberOfTimesDfsCalled++;
-                DFS(adj,i,visited);
+                DFS(isConnected,i,visited);
             }
         }
         return numberOfTimesDfsCalled;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n=isConnected.size();
+//         vector<int> visited(n,false);
+//         queue<int> q;
+//         int numberOfTimesBfsCalled=0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 numberOfTimesBfsCalled++;
+//                 q.push(i);
+//                 visited[i]=true;
+//                 while(!q.empty()){
+//                     int u=q.front();q.pop();
+//                     for(int v=0;v<n;v++){
+//                         if(!visited[v] and isConnected[u][v]==1){
+//                             q.push(v);
+//                             visited[v]=true;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return numberOfTimesBfsCalled;
+//     }
+// };
