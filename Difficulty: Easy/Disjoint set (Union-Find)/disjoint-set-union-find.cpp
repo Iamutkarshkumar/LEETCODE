6e9a364f3da475020ff884c8be514d645a6a4773
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-22 12:37:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
@@ -1,11 +1,25 @@
-/*Complete the functions below*/
-int find(int par[], int x) {
-    if(x==par[x]) return x;
-    return par[x]=find(par,par[x]);
-}
-
-void unionSet(int par[], int x, int z) {
-    int p_x=find(par,x);
-    int p_z=find(par,z);
-    if(p_x!=p_z) par[p_x]=p_z;
-}
+class Solution {
+  public:
+    vector<int> parent,rank;
+    int find(int i){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i]);
+    }
+    void Union(int x,int y){
+        int parX=find(x);
+        int parY=find(y);
+        if(parX!=parY) parent[parX]=parY;
+    }
+    vector<int> DSU(int n, vector<vector<int>>& queries) {
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=0;i<=n;i++) parent[i]=i;
+        
+        vector<int> ans;
+        for(vector<int>& vec: queries){
+            if(vec[0]==1) Union(vec[1],vec[2]);
+            else ans.push_back(find(vec[1]));
+        }
+        return ans;
+    }
+};
