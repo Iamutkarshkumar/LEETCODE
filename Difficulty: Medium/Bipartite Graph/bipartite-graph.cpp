5229941919a298b4f277378fb4fd8a2e42546466
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-22 12:01:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool DFS(vector<vector<int>>& adj,int u,vector<int>& color,int currColor){
+        color[u]=currColor;
+        for(int v: adj[u]){
+            if(color[u]==color[v]) return false;
+            if(color[v]==-1){
+                if(DFS(adj,v,color,1-currColor)==false) return false;
+            }
+        }
+        return true;
+    }
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(V);
+        for(auto ele: edges){
+            int u=ele[0],v=ele[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int> color(V,-1);
+        for(int i=0;i<V;i++){
+            if(color[i]==-1 and DFS(adj,i,color,1)==false) return false;
+        } 
+        return true;
+    }
+};
