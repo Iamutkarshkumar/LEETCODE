909eb0f278201da7eedb93704c16c09ebc16b5e9
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-22 12:17:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,11 +1,14 @@
 class Solution {
   public:
-    bool DFS(vector<vector<int>>& adj,int u,vector<int>& color,int currColor){
+    bool BFS(vector<vector<int>>& adj,int u,vector<int>& color,int currColor){
         color[u]=currColor;
-        for(int v: adj[u]){
-            if(color[u]==color[v]) return false;
-            if(color[v]==-1){
-                if(DFS(adj,v,color,1-currColor)==false) return false;
+        queue<int> q;
+        q.push(u);
+        while(!q.empty()){
+            int u=q.front();q.pop();
+            for(int v: adj[u]){
+                if(color[v]==color[u]) return false;
+                if(color[v]==-1 and BFS(adj,v,color,1-currColor)==false) return false;
             }
         }
         return true;
@@ -17,11 +20,34 @@ class Solution {
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        
         vector<int> color(V,-1);
         for(int i=0;i<V;i++){
-            if(color[i]==-1 and DFS(adj,i,color,1)==false) return false;
-        } 
+            if(color[i]==-1 and BFS(adj,i,color,1)==false) return false;
+        }
         return true;
     }
 };
+// class Solution {
+//   public:
+//     bool DFS(vector<vector<int>>& adj,int u,vector<int>& color,int currColor){
+//         color[u]=currColor;
+//         for(int v: adj[u]){
+//             if(color[u]==color[v]) return false;
+//             if(color[v]==-1 and DFS(adj,v,color,1-currColor)==false) return false;
+//         }
+//         return true;
+//     }
+//     bool isBipartite(int V, vector<vector<int>> &edges) {
+//         vector<vector<int>> adj(V);
+//         for(auto ele: edges){
+//             int u=ele[0],v=ele[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         vector<int> color(V,-1);
+//         for(int i=0;i<V;i++){
+//             if(color[i]==-1 and DFS(adj,i,color,1)==false) return false;
+//         } 
+//         return true;
+//     }
+// };
