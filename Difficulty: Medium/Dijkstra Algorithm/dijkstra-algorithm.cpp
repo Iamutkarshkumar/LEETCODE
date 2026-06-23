073e2aa932ba540935106b78c07077de4f0ac022
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-23 18:09:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,31 +1,40 @@
-// Input: V = 3, edges[][] = [[0, 1, 1], [1, 2, 3], [0, 2, 6]], source = 2
-// Output: [4, 3, 0]
+//using min heap 
 class Solution {
   public:
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int source) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> result(V,INT_MAX);
-        result[source]=0;
-        pq.push({0,source});
-        vector<vector<pair<int, int>>> adj(V);
-        for(auto &edge : edges) {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]}); // Assuming undirected graph
-        }
+    void Dijkstra(
+        vector<vector<pair<int,int>>>& adj,
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>& pq,
+        vector<int>& result,
+        int src
+    ){
+        result[src]=0;
+        pq.push({0,src});
         while(!pq.empty()){
-            int d=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            for(auto &vec: adj[node]){
-                int nextNode= vec.first;
-                int wt= vec.second;
-
-                if(d+wt < result[nextNode]) {
-                    result[nextNode] = d + wt;
-                    pq.push({d+wt, nextNode});
+            auto ele =pq.top();pq.pop();
+            int d=ele.first;
+            int u=ele.second;
+            for(auto ele: adj[u]){
+                int dist=ele.second;
+                int v=ele.first;
+                if(d+dist<result[v]){
+                    result[v]=d+dist;
+                    pq.push({d+dist,v});
                 }
             }
         }
+    }
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto ele: edges){
+            adj[ele[0]].push_back({ele[1],ele[2]});
+            adj[ele[1]].push_back({ele[0],ele[2]});
+        } 
+        
+        // pair -> dist , node
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> result(V,INT_MAX);
+        Dijkstra(adj,pq,result,src);
         return result;
     }
 };
