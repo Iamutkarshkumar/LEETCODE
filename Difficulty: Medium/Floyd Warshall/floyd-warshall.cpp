13241f7411647d3ebb3f06311ec2fe11b7a2fff7
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-28 15:38:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -1,15 +1,14 @@
 class Solution {
   public:
-	void floydWarshall(vector<vector<int>>& grid){
-        int n = grid.size();
-        for(int via = 0; via < n; via++) {
-            for(int i = 0; i<n; i++) {
-                if (grid[i][via] == 1e8) continue;
-                for(int j = 0; j<n; j++) {
-                    if (grid[via][j] == 1e8) continue;
-                    grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+    void floydWarshall(vector<vector<int>> &grid) {
+        int n=grid.size();
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(grid[i][via]!=1e8 && grid[via][j]!=1e8)
+                    grid[i][j]=min(grid[i][j] , grid[i][via]+grid[via][j]);
                 }
             }
         }
-	}
+    }
 };
