3be6c7cd92d5240852364ee754c986b1e95071cb
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-06-30 10:47:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Euler circuit and Path/euler-circuit-and-path.cpp
+++ Difficulty: Medium/Euler circuit and Path/euler-circuit-and-path.cpp
@@ -1,70 +1,38 @@
-//Eularian path:
-//a path that visits every edge exactly once
-//not all graphs have Eulerian paths
-//be careful from which vertex you start
-
-//Eulerian Circuit:
-// an EP that starts and ends at the same vertex
-//also known as "Eulerian Cycle"
-//not all graphs have Eulerian circuits
-//if the graph has EC then you can start path/circuit from any vertex
-
-//all vertices with non-zero degree are connected
-
-//indegree importance:
-// EC ---> all vertices have even indegree
-// EP ---> exactly 0 or 2 vertices have odd indegree
-
-//semi Eulerian Graph:
-// a graph that has an Eulerian path but not an Eulerian circuit
-
-
-//if odd deg count == 2 then it has an Eulerian path
-//if odd deg count > 2 then it has no Eulerian path or circuit
-//if odd deg count == 0 then it has an Eulerian circuit
-
-
 class Solution {
   public:
-    void DFS(int u, vector<int> adj[], vector<bool>& visited) {
-        visited[u] = true;
-        for (auto v : adj[u]) {
-            if (!visited[v]) {
-                DFS(v, adj, visited);
-            }
+    void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited){
+        visited[u]=true;
+        for(int v: adj[u]){
+            if(!visited[v]) DFS(adj,v,visited);
         }
     }
-    bool isConnected(int V, vector<int> adj[]){
-        int nonZeroDegreeVertex = -1;
+    bool isConnected(int V, vector<vector<int>>& adj){
+        int nonZeroDegVertex=-1;
         for(int i=0;i<V;i++){
             if(adj[i].size()>0){
-                nonZeroDegreeVertex = i;
+                nonZeroDegVertex=i;
                 break;
             }
         }
-
         vector<bool> visited(V,false);
-        DFS(nonZeroDegreeVertex,adj,visited);
-
+        //start dfs fron non zero deg index to mark all non zero vertex in that component
+        // if all the non zero deg vertex are not in same component then graph cant have euler path
+        DFS(adj,nonZeroDegVertex,visited);
+        //check all non zero deg vertex marked visited or not
         for(int i=0;i<V;i++){
-            if(visited[i]==false and adj[i].size()>0){
-                return false; // not all vertices with non-zero degree are connected
-            }
+            if(visited[i]==false and adj[i].size()>0) return false;
         }
-        return true; // all vertices with non-zero degree are connected
+        return true;
     }
-    int isEulerCircuit(int V, vector<int> adj[]) {
+    int isEulerCircuit(int V, vector<vector<int>>& adj) {
         if(isConnected(V,adj)==false) return 0;
-
-        int oddDegCount = 0;
+        int oddDegCount=0;
         for(int i=0;i<V;i++){
-            if(adj[i].size() % 2 != 0){
-                oddDegCount++;
-            }
+           if(adj[i].size()%2==1) oddDegCount++;
         }
-       
-        if(oddDegCount > 2) return 0; // not an Eulerian path or circuit
-        if(oddDegCount == 2) return 1; // Eulerian path exists
-        if(oddDegCount == 0) return 2; // Eulerian circuit exists
+        if(oddDegCount>2) return 0;
+        if(oddDegCount==2) return 1;
+        return 2;
+        
     }
 };
