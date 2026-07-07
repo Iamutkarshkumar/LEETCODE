7746b0e1fe1ad52cccc5edba39d8346b6f6f3cde
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-07 18:13:00 +0530

    Time: 0 ms (100%), Space: 10.2 MB (33.49%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,43 +1,87 @@
+// class MyQueue {
+// public:
+//     stack<int> st1,st2;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) {
+//         st1.push(x);
+//     }
+    
+//     int pop() {
+//         while(!st1.empty()){
+//             st2.push(st1.top());
+//             st1.pop();
+//         }
+//         int x=st2.top();
+//         st2.pop();
+//         while(!st2.empty()){
+//             st1.push(st2.top());
+//             st2.pop();
+//         }
+//         return x;
+//     }
+    
+//     int peek() {
+//         while(!st1.empty()){
+//             st2.push(st1.top());
+//             st1.pop();
+//         }
+//         int x=st2.top();
+//         while(!st2.empty()){
+//             st1.push(st2.top());
+//             st2.pop();
+//         }
+//         return x;
+//     }
+    
+//     bool empty() {
+//         return st1.size()==0;
+//     }
+// };
+
+// method 2
+//T.C : push - O(1), pop - Amortized O(1) , peek() - O(1), empty() - O(1)
+//S.C : O(n)
 class MyQueue {
 public:
-    stack<int> st1,st2;
+    stack<int> input,output;
+    int peekEle=-1;
     MyQueue() {
         
     }
     
     void push(int x) {
-        st1.push(x);
+        if(input.empty()) peekEle=x;
+        input.push(x);
     }
     
     int pop() {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
+        if(!output.empty()){
+            int temp=output.top();
+            output.pop();
+            return temp;
+        } 
+        else{
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+            int temp=output.top();
+            output.pop();
+            return temp;
         }
-        int x=st2.top();
-        st2.pop();
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
-        return x;
+        return -1;
     }
     
     int peek() {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-        int x=st2.top();
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
-        return x;
+        if(output.empty()) return peekEle;
+        return output.top();
     }
     
     bool empty() {
-        return st1.size()==0;
+        return input.empty() and output.empty();
     }
 };
 
