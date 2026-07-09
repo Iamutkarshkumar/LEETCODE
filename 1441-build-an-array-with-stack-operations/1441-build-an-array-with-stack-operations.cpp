f17029edeef05bdba85a45827ae7dcfc12a31d90
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-09 18:47:37 +0530

    Time: 2 ms (9.68%), Space: 10.7 MB (56.2%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -2,15 +2,36 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        for(int i=1;i<=min(n,target[target.size()-1]);i++){
-            if(binary_search(begin(target),end(target),i)){
-                ans.push_back("Push");
+        int i=0,stream=1;
+        while(i<target.size() and stream<=n){
+            ans.push_back("Push");
+            if(target[i]==stream){
+                i++;
+                stream++;
             }
             else{
-                ans.push_back("Push");
                 ans.push_back("Pop");
+                stream++;
             }
         }
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<string> buildArray(vector<int>& target, int n) {
+//         vector<string> ans;
+//         unordered_set<int> s;
+//         for(auto ele: target) s.insert(ele);
+//         for(int i=1;i<=min(n,target[target.size()-1]);i++){
+//             if(s.count(i)){
+//                 ans.push_back("Push");
+//             }
+//             else{
+//                 ans.push_back("Push");
+//                 ans.push_back("Pop");
+//             }
+//         }
+//         return ans;
+//     }
+// };
