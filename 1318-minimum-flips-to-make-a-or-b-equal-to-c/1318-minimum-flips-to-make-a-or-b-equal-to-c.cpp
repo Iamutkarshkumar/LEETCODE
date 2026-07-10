5b3334c9ae2ee241b3b613ee36f54694839fc17b
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-10 15:03:19 +0530

    Time: 3 ms (24.79%), Space: 7.7 MB (92.99%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,21 +1,30 @@
+
+//approach 1
+// class Solution {
+// public:
+//     int minFlips(int a, int b, int c) {
+//         int ans=0;
+//         while(a or b or c){
+//             int x=a&1;
+//             int y=b&1;
+//             int z=c&1;
+//             if(z){
+//                 if(x==0 and y==0) ans++;
+//             }
+//             else{
+//                 if(x) ans++;
+//                 if(y) ans++;
+//             }
+//             c>>=1;
+//             a>>=1;
+//             b>>=1;    
+//         } 
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int ans=0;
-        while(a or b or c){
-            int x=a&1;
-            int y=b&1;
-            int z=c&1;
-            if(z){
-                if(x==0 and y==0) ans++;
-            }
-            else{
-                ans+=x+y;
-            }
-            c>>=1;
-            a>>=1;
-            b>>=1;    
-        } 
-        return ans;
+        return __builtin_popcount((a|b)^c) + __builtin_popcount((a&b) & ((a|b)^c));
     }
 };
