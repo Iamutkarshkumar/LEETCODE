5d699d56ad8d00d5a63193042af48c3be5a666bd
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-24 08:26:08 +0530

    Time: 627 ms (65.17%), Space: 35.4 MB (80.9%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,18 +1,46 @@
+// class Solution {
+// public:
+//     int uniqueXorTriplets(vector<int>& nums) {
+//         int n=nums.size();
+//         unordered_set<int> s1,s2;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 s1.insert(nums[i]^nums[j]);
+//             }
+//         }
+//         for(auto ele: s1){
+//             for(int k=0;k<n;k++){
+//                 s2.insert(nums[k]^ele);
+//             }
+//         }
+//         return s2.size();
+//     }
+// };
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n=nums.size();
-        unordered_set<int> s1,s2;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                s1.insert(nums[i]^nums[j]);
+        int n = nums.size();
+
+        vector<bool> s1(2048, false), s2(2048, false);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s1[nums[i] ^ nums[j]] = true;
             }
         }
-        for(auto ele: s1){
-            for(int k=0;k<n;k++){
-                s2.insert(nums[k]^ele);
+
+        for (int x = 0; x < 2048; x++) {
+            if (!s1[x]) continue;
+
+            for (int k = 0; k < n; k++) {
+                s2[x ^ nums[k]] = true;
             }
         }
-        return s2.size();
+
+        int cnt = 0;
+        for (bool x : s2)
+            cnt += x;
+
+        return cnt;
     }
 };
