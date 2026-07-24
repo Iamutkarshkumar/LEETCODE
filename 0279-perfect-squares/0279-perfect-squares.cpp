59276574d2fcdd30087b1f29cfc035928380ba3e
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-24 19:52:57 +0530

    Time: 75 ms (42.36%), Space: 9.1 MB (91.55%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dp[10001];
+    int solve(int n){
+        if(n==0) return 0;
+        if(dp[n]!=-1) return dp[n];
+
+        int rootOfN=floor(sqrt(n));
+        int ans=n;
+        for(int i=1;i<=rootOfN;i++) ans=min(ans,1+solve(n-i*i));
+        return dp[n]=ans;
+    }
+    int numSquares(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
