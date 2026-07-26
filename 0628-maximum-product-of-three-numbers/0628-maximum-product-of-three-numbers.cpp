698c3df77dca18e0f0de29702268e1fe571ba4ae
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-26 07:00:08 +0530

    Time: 0 ms (100%), Space: 31.6 MB (46.18%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,8 +1,42 @@
+// method 1: tc O(nlogn)
+// class Solution {
+// public:
+//     int maximumProduct(vector<int>& nums) {
+//         int n=nums.size();
+//         sort(begin(nums),end(nums));
+//         return max(nums[0]*nums[1]*nums[n-1] , nums[n-3]*nums[n-2]*nums[n-1]);
+//     }
+// };
+
+//method 2: O(n)
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int n=nums.size();
-        sort(begin(nums),end(nums));
-        return max(nums[0]*nums[1]*nums[n-1] , nums[n-3]*nums[n-2]*nums[n-1]);
+        int firstMax=INT_MIN,secondMax=INT_MIN,thirdMax=INT_MIN;
+        int firstMin=INT_MAX,secondMin=INT_MAX;
+        for(int ele: nums){
+            if(ele>firstMax){
+                thirdMax=secondMax;
+                secondMax=firstMax;
+                firstMax=ele;
+            }
+            else if(ele>secondMax){
+                thirdMax=secondMax;
+                secondMax=ele;
+            }
+            else if(ele>thirdMax){
+                thirdMax=ele;
+            }
+
+            if(ele<firstMin){
+                secondMin=firstMin;
+                firstMin=ele;
+            }
+            else if(ele<secondMin){
+                secondMin=ele;
+            }
+        }
+        return max(firstMin*secondMin*firstMax, firstMax*secondMax*thirdMax);
     }
 };
