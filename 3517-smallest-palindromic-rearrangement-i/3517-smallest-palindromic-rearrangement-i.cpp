00ef1b1331c74b5d03ebd40b47b187eead4dfa49
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-07-28 08:28:12 +0530

    Time: 32 ms (64.4%), Space: 73.3 MB (48.35%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,11 +1,33 @@
+// class Solution {
+// public:
+//     string smallestPalindrome(string s) {
+//         int n=s.length();
+//         int pivot=n/2;
+//         sort(s.begin(),s.begin()+pivot);
+//         sort(s.end()-pivot,s.end());
+//         reverse(s.end()-pivot,s.end());
+//         return s;
+//     }
+// };
 class Solution {
 public:
     string smallestPalindrome(string s) {
         int n=s.length();
-        int pivot=n/2;
-        sort(s.begin(),s.begin()+pivot);
-        sort(s.end()-pivot,s.end());
-        reverse(s.end()-pivot,s.end());
-        return s;
+        vector<int> freq(26,0);
+        for(auto ele: s) freq[ele-'a']++;
+        string ans="";
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                int x=freq[i]/2;
+                freq[i]-=2*x;
+                while(x--) ans+=(i+'a');
+            }
+        }
+        string temp=ans;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0) ans+=(i+'a');
+        }
+        reverse(begin(temp),end(temp));
+        return ans+temp;
     }
 };
