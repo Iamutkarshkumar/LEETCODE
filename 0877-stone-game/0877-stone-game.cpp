da157bda8d3519e1d6aa2abde3289a688f92b3a3
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-08-02 08:11:33 +0530

    Time: 11 ms (27.05%), Space: 11.6 MB (36.14%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[501][501];
+    int solve(int l,int r,vector<int>& piles){
+        if(l>r) return 0;
+        if(dp[l][r]!=-1) return dp[l][r];
+
+        int takeleft=piles[l]+min(solve(l+2,r,piles),solve(l+1,r-1,piles));
+        int takeright=piles[r]+min(solve(l+1,r-1,piles),solve(l,r-2,piles));
+
+        return dp[l][r]=max(takeleft,takeright);
+    }
+    bool stoneGame(vector<int>& piles) {
+        memset(dp,-1,sizeof(dp));
+        int n=piles.size();
+        int total=0;
+        for(int &ele: piles) total+=ele;
+        int aliceStones = solve(0,n-1,piles);
+        int bobStones = total-aliceStones;
+        return aliceStones>bobStones;
+        
+    }
+};
