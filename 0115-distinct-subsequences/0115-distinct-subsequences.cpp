7d0a256b6f849e9658ee7ed579128342f34a4cea
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-09-06 06:50:02 +0530

    Time: 34 ms (36.99%), Space: 27.1 MB (52.57%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int n,m;
-    unordered_map<string,int> dp;
+    vector<vector<int>> dp;
     int solve(int i,int j,string& s,string& t){
         //base case
         if(j==m) return 1;
         if(i==n) return 0;
         //memoization 
-        string key = to_string(i) + "_" + to_string(j);
-        if(dp.count(key)) return dp[key];
+        if(dp[i][j]!=-1) return dp[i][j];
 
         int skip=solve(i+1,j,s,t);
         int take=0;
         if(s[i]==t[j]) take=solve(i+1,j+1,s,t);
 
-        return dp[key]=take+skip;
+        return dp[i][j]=take+skip;
     }
     int numDistinct(string s, string t) {
         n=s.length();
         m=t.length();
+        dp.assign(n,vector<int>(m,-1));
         return solve(0,0,s,t);
     }
 };
