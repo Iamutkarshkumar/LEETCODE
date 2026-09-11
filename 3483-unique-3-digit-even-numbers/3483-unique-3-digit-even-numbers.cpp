9ec28244d1d97b6afd1ef83b4b9bc4b6b2531632
Utkarsh kumar <146476796+Iamutkarshkumar@users.noreply.github.com>
2026-09-11 09:31:09 +0530

    Time: 0 ms (100%), Space: 28.6 MB (93.75%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    vector<int> freq;
-    bool check(int x){
-        vector<int> v(10,0);
-        while(x){
-            int last=x%10;
-            v[last]++;
-            x/=10;
-        }
-        for(int i=0;i<10;i++)
-            if(v[i]>freq[i]) return false;
-
-        return true;
-    }
     int totalNumbers(vector<int>& digits) {
-        freq.assign(10,0);
-        for(auto ele: digits) freq[ele]++;
-
+        vector<int> freq(10,0);
         int ans=0;
-        for(int i=100;i<=998;i+=2){
-            if(check(i)) ans++;
+
+        for(int d:digits) freq[d]++;
+
+        for(int u=0;u<=8;u+=2){
+            if(!freq[u]) continue;
+
+            freq[u]--;
+
+            for(int h=1;h<=9;h++){
+                if(!freq[h]) continue;
+
+                freq[h]--;
+
+                for(int t=0;t<=9;t++)
+                    if(freq[t]) ans++;
+
+                freq[h]++;
+            }
+
+            freq[u]++;
         }
+
         return ans;
     }
 };
